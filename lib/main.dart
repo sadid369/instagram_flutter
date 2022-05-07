a1de622a94439ee70c82577f052647470d7b0843
Sadid369 <sadid.jones@gmail.com>
2022-05-07 22:15:25 +0600

    5/7/2022 @ 33:14

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,29 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_scrren.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyDTCCNk_77P-B34_oO5orRhsUTbNx-iP28",
+        appId: "1:601866920862:web:cbf64ca1c9bd814121ec8f",
+        messagingSenderId: "601866920862",
+        projectId: "shop-app-scratch",
+        storageBucket: "shop-app-scratch.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -23,7 +40,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      home: ResponsiveLayout(
+      home: const ResponsiveLayout(
           webScreenLayout: WebScreenLayout(),
           mobileScreenLayout: MobileScreenLayout()),
     );
